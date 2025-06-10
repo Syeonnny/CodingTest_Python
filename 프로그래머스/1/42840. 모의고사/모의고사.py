93c6d7fc37f783f476d42a72dec7236e03b21b33
seungyeon <tmddus6901@naver.com>
2025-06-10 21:01:44 +0900

    [level 1] Title: 모의고사, Time: 5.90 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,26 @@
 def solution(answers):
     answer = []
-    one = [1,2,3,4,5] 
-    two = [2,1,2,3,2,4,2,5]
-    three = [3,3,1,1,2,2,4,4,5,5] 
-    one_count, two_count, three_count = 0, 0, 0 
+    length = len(answers)
+    one = [1,2,3,4,5]*length
+    two = [2,1,2,3,2,4,2,5]*length
+    three = [3,3,1,1,2,2,4,4,5,5]*length
     
-    for i in range(len(answers)):
-        if answers[i] == one[i%len(one)]:
-            one_count += 1
-        if answers[i] == two[i%len(two)]:
-            two_count += 1
-        if answers[i] == three[i%len(three)]:
-            three_count += 1    
-            
-    max_count = max(one_count, two_count, three_count)
-    if max_count == one_count:
+    one_counts, two_counts, three_counts = 0,0,0
+    for i in range(length):
+        if one[i] == answers[i]:
+            one_counts += 1
+    for i in range(length):
+        if two[i] == answers[i]:
+            two_counts += 1
+    for i in range(length):
+        if three[i] == answers[i]:
+            three_counts += 1
+    
+    max_num = max(one_counts, two_counts, three_counts)
+    if max_num == one_counts:
         answer.append(1)
-    if max_count == two_count:
+    if max_num == two_counts:
         answer.append(2)
-    if max_count == three_count:
+    if max_num == three_counts:
         answer.append(3)
-    
     return answer
