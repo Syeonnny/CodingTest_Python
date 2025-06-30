fabfea12730b9d89f03134a81b637c57bdcac4b8
seungyeon <tmddus6901@naver.com>
2025-06-30 16:45:17 +0900

    [level 2] Title: 주식가격, Time: 146.85 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 18.5 MB, 시간: 144.38 ms
+메모리: 18.6 MB, 시간: 146.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 16일 17:23:58
+2025년 06월 30일 16:45:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,10 @@
 def solution(prices):
     answer = []
     for i in range(len(prices)):
-        count = 0
+        count = 0 
         for j in range(i+1, len(prices)):
-            if prices[i]<=prices[j]:
-                count += 1
-            else:
-                count += 1
+            count += 1
+            if prices[i] > prices[j]:
                 break
         answer.append(count)
     return answer
