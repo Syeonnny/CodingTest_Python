4104f17f1acd80edc1a029464618ecf65e3d6cc6
seungyeon <tmddus6901@naver.com>
2025-07-29 02:06:35 +0900

    [level 2] Title: 구명보트, Time: 12.76 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    people = deque(sorted(people))
+    while people:
+        if len(people) >= 2 and people[0] + people[-1] <= limit:
+            people.popleft()
+            people.pop()
+        else: 
+            people.pop()
+        answer += 1
+    return answer
