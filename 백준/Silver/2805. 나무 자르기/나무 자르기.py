eaa136defede32132b603406b32bcdbb2d6f7494
seungyeon <tmddus6901@naver.com>
2025-08-03 16:29:30 +0900

    [Silver II] Title: 나무 자르기, Time: 444 ms, Memory: 258492 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split())
+array = list(map(int, input().strip().split()))
+
+start = 0 
+end = max(array)
+result = 0 
+
+while start <= end: 
+    total = 0
+    mid = (start + end) // 2
+    
+    for i in array:
+        if i > mid:
+            total += i - mid
+       
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
