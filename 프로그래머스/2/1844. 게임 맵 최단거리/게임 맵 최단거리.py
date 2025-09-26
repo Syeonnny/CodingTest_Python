918a0246cf47ede083aa78a3f54f17aac77738af
seungyeon <tmddus6901@naver.com>
2025-09-27 02:20:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.57 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,25 @@
 from collections import deque
 def solution(maps):
-    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
-    n,m  = len(maps), len(maps[0])
-    directions = [(-1,0), (1,0), (0,-1), (0,1)]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    def bfs():
-        queue = deque([(0,0,1)])
-        while queue:
-            curr = queue.popleft()
-            x, y, dist = curr
-            if x == n-1 and y == m-1:
-                return dist
-            else:
-                for dx, dy in directions:
-                    nx = x + dx
-                    ny = y + dy
-                    if 0 <= nx < n and 0 <= ny < m:
-                        if maps[nx][ny] == 1 and not visited[nx][ny]:
-                            visited[nx][ny] = True
-                            queue.append((nx, ny, dist+1))
-        return -1
-    return bfs()
+    n = len(maps)
+    m = len(maps[0])
+    
+    visited =[[False]*m for _ in range(n)]
+    queue = deque([(0, 0, 1)])
+    visited[0][0] = True
+    
+    while queue: 
+        x, y, dist = queue.popleft()
+        
+        if x == n-1 and y ==m-1:
+            return dist
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if not visited[nx][ny] and maps[nx][ny] == 1: 
+                    visited[nx][ny] = True
+                    queue.append((nx, ny, dist+1))
+    return -1
