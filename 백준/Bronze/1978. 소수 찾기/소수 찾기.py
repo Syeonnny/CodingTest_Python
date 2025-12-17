c7de8f7b16113e2d1ea64c9992784e2cdeaa5b65
seungyeon <tmddus6901@naver.com>
2025-12-17 17:55:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+N = int(input())
+nums = map(int, input().split())
+count = 0
+
+for n in nums:
+    if n < 2:
+        continue
+
+    is_prime = True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        count += 1
+
+print(count)
