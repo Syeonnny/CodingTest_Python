b9ee98d473816c217058d6b417bb277d6c966d4f
seungyeon <tmddus6901@naver.com>
2025-12-17 16:01:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,7 +1,4 @@
 a, b, c = [int(input()) for _ in range(3)]
-num = str(a * b * c)
-numslist = {}
-for i in num:
-    numslist[i] = numslist.get(i, 0) + 1
-for i in range(10): 
-    print(numslist.get(str(i), 0))
+s = str(a*b*c)
+for d in range(10):
+    print(s.count(str(d)))
