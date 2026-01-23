44917c586064af495bac52bc098f5df7a5c983bb
seungyeon <tmddus6901@naver.com>
2026-01-23 20:44:56 +0900

    [Silver III] Title: 시리얼 번호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -1,13 +1,12 @@
 N = int(input())
 guitar = [input() for _ in range(N)]
-number = [str(i) for i in range(1,10)]
 
 def sum_of_digits(s):
     total = 0
-    for i in s:
-        if i in number:
-            total += int(i)
+    for c in s:
+        if c.isdigit():
+            total += int(c)
     return total
-    
+
 guitar = sorted(guitar, key=lambda x:(len(x), sum_of_digits(x), x))
 print(*guitar, sep='\n')
