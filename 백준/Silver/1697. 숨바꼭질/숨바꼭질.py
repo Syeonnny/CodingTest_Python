ae3cbf540f32717c6a3095a126ecd6c87926c236
seungyeon <tmddus6901@naver.com>
2026-02-15 21:23:49 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 38780 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,20 +1,26 @@
 from collections import deque
 
-N, K = map(int,input().split())
-Max = 10**6
-visited = [False] * (Max+1)
+N, K = map(int, input().split())
+dist = [0] * 100001
+visited = [False] * 100001 
+
+if N == K:
+    print(0)
+    exit()
 
 def bfs(x):
-    queue = deque()
-    queue.append((x, 0)) 
-    while queue:
-        n, sec = queue.popleft()
-        if n == K:
-            return sec
-        
-        for nx in (n-1, n+1, n*2):
-            if 0 <= nx <= Max and not visited[nx]:
-                visited[nx] = True
-                queue.append((nx, sec+1))
+    visited[x] = True
+    queue = deque([x])
+    
+    while queue: 
+        x = queue.popleft()
 
+        for nx in (x-1, x+1, 2*x):
+            if 0 <= nx <= 100000 and not visited[nx]:
+                visited[nx] = True
+                queue.append(nx)
+                dist[nx] = dist[x]+ 1
+                if nx == K: 
+                    return dist[nx]
+        
 print(bfs(N))
