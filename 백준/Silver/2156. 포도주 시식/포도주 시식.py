8f6dc2713a69cc431cff11b149b2e1a606582b39
seungyeon <tmddus6901@naver.com>
2026-02-15 22:22:39 +0900

    [Silver I] Title: 포도주 시식, Time: 276 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,18 +1,20 @@
 n = int(input())
-cups = [int(input()) for _ in range(n)]
-
+q = [int(input()) for _ in range(n)]
 if n == 1:
-    print(cups[0])
+    print(q[0])
 elif n == 2:
-    print(cups[0]+cups[1])
+    print(q[0]+q[1])
 else: 
-    dp = [0] * n
-    dp[0] = cups[0]
-    dp[1] = cups[0] + cups[1]
-    dp[2] = max(cups[0] + cups[2], cups[1] + cups[2], dp[1])
+    dp = [0] * n 
+    dp[0] = q[0]
+    dp[1] = q[0] + q[1]
+    dp[2] = max(q[0] + q[1], 
+                q[1] + q[2],
+                q[0] + q[2])
 
     for i in range(3, n):
-        dp[i] = max(dp[i-3] + cups[i-1] + cups[i], 
-                    dp[i-2] + cups[i],
-                    dp[i-1])
+        dp[i] = max(dp[i-1], 
+                    dp[i-2] + q[i], 
+                    dp[i-3] + q[i-1] + q[i]) 
+    
     print(dp[n-1])
