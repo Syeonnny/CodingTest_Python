eb9f6d3e641ea01335b5f393933fcbbc9d28b10a
seungyeon <tmddus6901@naver.com>
2026-02-15 17:06:50 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,33 +1,31 @@
 from collections import deque
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 
-graph = [list(map(int, input().strip())) for _ in range(N)]
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().strip())))
+visited = [[False] * m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
-dist = [[0]*M for _ in range(N)]
-
-def bfs(x, y):
-    queue = deque() 
-    queue.append((x,y))
-    dist[x][y] = 1 
+def bfs(x, y, graph):
+    queue = deque([(x,y)])
+    visited[x][y] = True
 
     while queue:
         x, y = queue.popleft()
 
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= N or ny >= M:
-                continue
-        
-            if graph[nx][ny] == 1 and dist[nx][ny] == 0:
-                queue.append((nx, ny))           
-                dist[nx][ny] = dist[x][y] + 1
+            if 0 <= nx < n and 0 <= ny < m:
+                if visited[nx][ny] == False and graph[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                    graph[nx][ny] = graph[x][y] + 1
 
-    return dist[N-1][M-1]
+    return graph[n-1][m-1]
 
-print(bfs(0,0))
+print(bfs(0,0,graph))
